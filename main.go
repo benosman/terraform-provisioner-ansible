060e753f55b7275e798d83b7541f1508d6ea0be1
Ben Osman <dev@smoothify.com>
2019-09-19 18:16:46 +0100

    reorganise provisioner file location

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/radekg/terraform-provisioner-ansible/ansible"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProvisionerFunc: func() terraform.ResourceProvisioner {
-			return Provisioner()
+			return ansible.Provisioner()
 		},
 	})
 }
